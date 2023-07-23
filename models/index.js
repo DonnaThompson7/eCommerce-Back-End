b016b44d1752bd01cdae71a67461d195c05680f9
DonnaThompson7 <130340823+DonnaThompson7@users.noreply.github.com>
2023-07-23 11:09:15 -0400

    fixed syntax for Many-to-Many relationship

--- models/index.js
+++ models/index.js
@@ -15,10 +15,16 @@ Category.hasMany(Product, {
 });
 
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, {through: "ProductTag"});
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, {through: "ProductTag"});
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
 
 module.exports = {
   Product,
